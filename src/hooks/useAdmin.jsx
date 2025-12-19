4057bd2c54b64b9fd62722f64ebc8595de898ab0
mhmasum1 <mhmasum231@gmail.com>
2025-12-19 11:33:55 +0600

    implement useAdmin hook

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useAdmin = () => {
+    const { user, loading } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isAdminLoading, setIsAdminLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAdmin = async () => {
+            if (!user?.email) {
+                setIsAdmin(false);
+                setIsAdminLoading(false);
+                return;
+            }
+
+            try {
+                setIsAdminLoading(true);
+                const res = await axiosSecure.get(`/users/admin/${user.email}`);
+                setIsAdmin(!!res.data?.admin);
+            } catch (err) {
+                setIsAdmin(false);
+            } finally {
+                setIsAdminLoading(false);
+            }
+        };
+
+        if (!loading) checkAdmin();
+    }, [user?.email, loading, axiosSecure]);
+
+    return [isAdmin, isAdminLoading];
+};
+
+export default useAdmin;
